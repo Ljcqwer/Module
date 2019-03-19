266c59ab7381ca39af901fad6766ec4020cb12b4
ljcqwer <18352863902@163.com>
2019-03-19 15:07:01 +0800

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int i = 0;
+        if (i==0){
+            System.out.println("我将成为天国的壁垒，感受来自天国的悲痛吧---天国悲虹");
+        }else {
+            System.out.println("我将成为地狱的使徒，倾听来自地狱的挽歌吧---司夜统治");
+        }
+
     }
 }
